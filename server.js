7068131677fb3c0a6cfe31466b1100f9622f463e
adabotcon <adabotcon@outlook.com>
2017-03-02 14:00:28 -0800

    Blog init

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const jsonParser = bodyParser.json();
+const app = express();
+// log the http layer
+app.use(morgan('common'));
+
+const {BlogPosts} = require('./models');
+
+// create blogpost example
+BlogPosts.create('Example', 'This is an example post.', 'adabotcon', '11/21');
+BlogPosts.create('Example 2', 'This is an example post v2.', 'adabotcon', '11/22');
+
+// get posts
+app.get('/blog-posts', (req, res) => {
+	res.json(BlogPosts.get());
+});
+
+app.post('/blog-posts', jsonParser, (req, res) => {
+	//ensure correct info
+	const requiredFields = ['title', 'content', 'author', 'publishDate'];
+	for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+  res.status(201).json(item);
+});
+
+app.put('blog-posts/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author', 'publishDate', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post item \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.name,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).json(updatedItem);
+});
+
+app.delete('/blog-posts/:id', (req, res) => {
+  ShoppingList.delete(req.params.id);
+  console.log(`Deleted blogpost \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
