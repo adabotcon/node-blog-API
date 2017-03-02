35b64432a04e59ed41a9ec645650c10fd247f5db
adabotcon <adabotcon@outlook.com>
2017-03-02 14:11:11 -0800

    Adding modulizer

---
+++ blogPostsRouter.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+
+const {BlogPosts} = require('./models');
+
+router.get('/blog-posts', (req, res) => {
+	res.json(BlogPosts.get());
+});
+
+router.post('/blog-posts', jsonParser, (req, res) => {
+	//ensure correct info
+	const requiredFields = ['title', 'content', 'author', 'publishDate'];
+	for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+  res.status(201).json(item);
+});
+
+router.put('blog-posts/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author', 'publishDate', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post item \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.name,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).json(updatedItem);
+});
+
+router.delete('/blog-posts/:id', (req, res) => {
+  ShoppingList.delete(req.params.id);
+  console.log(`Deleted blogpost \`${req.params.ID}\``);
+  res.status(204).end();
+});
